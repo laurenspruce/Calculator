c124170e004a5165e2448a140340238c31de789e
Lauren <74539185+laurenspruce@users.noreply.github.com>
2023-10-08 20:12:46 +0100

    Updated with keystrokes for user to use

--- src/CalculatorFace.java
+++ src/CalculatorFace.java
@@ -1,2 +1,221 @@
-package PACKAGE_NAME;public class CalculatorFace {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorFace {
+    //All items relating to the GUI Form
+    private JTextField textDisplay;
+    private JPanel Calculator;
+    private JButton btnEquals;
+    private JButton btnMultiply;
+    private JButton btnTwo;
+    private JButton btnThree;
+    private JButton btnFive;
+    private JButton btnEight;
+    private JButton btnPoint;
+    private JButton btnPlus;
+    private JButton btnSix;
+    private JButton btnNine;
+    private JButton btnClear;
+    private JButton btnMinus;
+    private JButton btnDivide;
+    private JButton btnOne;
+    private JButton btnFour;
+    private JButton btnSeven;
+    private JButton btnZero;
+
+    //Setting totals for calculations
+    private double total1 = 0.0;
+    private double total2 = 0.0;
+    //Setting operations
+    private char mathOperator;
+
+    //Getting the character on which button was clicked
+    private void getOperator (String btnText) {
+        mathOperator = btnText.charAt(0);
+        total1 = total1 + Double.parseDouble(textDisplay.getText());
+        textDisplay.setText("");
+    }
+
+    public CalculatorFace() {
+
+        //All the buttons...
+        btnOne.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnOne, "1");
+                //Allows to keep adding 1 to the text display
+                String btnOneText = textDisplay.getText() + btnOne.getText();
+                textDisplay.setText(btnOneText);
+
+            }
+        });
+        btnTwo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnTwo, "2");
+                String btnTwoText = textDisplay.getText() + btnTwo.getText();
+                textDisplay.setText(btnTwoText);
+            }
+        });
+        btnThree.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnThree, "3");
+                String btnThreeText = textDisplay.getText() + btnThree.getText();
+                textDisplay.setText(btnThreeText);
+            }
+        });
+        btnFour.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnFour, "4");
+                String btnFourText = textDisplay.getText() + btnFour.getText();
+                textDisplay.setText(btnFourText);
+            }
+        });
+        btnFive.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnFive, "5");
+                String btnFiveText = textDisplay.getText() + btnFive.getText();
+                textDisplay.setText(btnFiveText);
+            }
+        });
+        btnSix.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnSix, "6");
+                String btnSixText = textDisplay.getText() + btnSix.getText();
+                textDisplay.setText(btnSixText);
+            }
+        });
+        btnSeven.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnSeven, "7");
+                String btnSevenText = textDisplay.getText() + btnSeven.getText();
+                textDisplay.setText(btnSevenText);
+            }
+        });
+        btnEight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnEight, "8");
+                String btnEightText = textDisplay.getText() + btnEight.getText();
+                textDisplay.setText(btnEightText);
+            }
+        });
+        btnNine.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnNine, "9");
+                String btnNineText = textDisplay.getText() + btnNine.getText();
+                textDisplay.setText(btnNineText);
+            }
+        });
+        btnZero.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnZero, "0");
+                String btnZeroText = textDisplay.getText() + btnZero.getText();
+                textDisplay.setText(btnZeroText);
+            }
+        });
+        btnPlus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonText = btnPlus.getText();
+                getOperator(buttonText);
+            }
+        });
+        btnEquals.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //Switch statement to see what operator was used
+                switch (mathOperator) {
+                    case '+':
+                        total2 = total1 + Double.parseDouble(textDisplay.getText());
+                        break;
+                    case '-':
+                        total2 = total1 - Double.parseDouble(textDisplay.getText());
+                        break;
+                    case '/':
+                        total2 = total1 / Double.parseDouble(textDisplay.getText());
+                        break;
+                    case '*':
+                        total2 = total1 * Double.parseDouble(textDisplay.getText());
+                }
+                textDisplay.setText(Double.toString(total2));
+                total1 = 0;
+            }
+        });
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                total2 = 0;
+                textDisplay.setText("");
+            }
+        });
+        btnPoint.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addKeyBinding(btnPoint, ".");
+                //Make the code neater '0.' instead of '.'
+                if (textDisplay.getText().equals("")) {
+                    textDisplay.setText("0.");
+                    //This will help to avoid a bug, '..' as well as '1..' or '.8.' is not acceptable for our simple calculator
+                } else if (textDisplay.getText().contains(".")) {
+                    btnPoint.setEnabled(false);
+                } else {
+                    String btnPointText = textDisplay.getText() + btnPoint.getText();
+                        textDisplay.setText(btnPointText);
+                }
+                btnPoint.setEnabled(true);
+            }
+        });
+        btnMinus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonText = btnMinus.getText();
+                getOperator(buttonText);
+            }
+        });
+        btnDivide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonText = btnDivide.getText();
+                getOperator(buttonText);
+            }
+        });
+        btnMultiply.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonText = btnMultiply.getText();
+                getOperator(buttonText);
+            }
+        });
+    }
+
+    // Create a method to add key bindings
+    private void addKeyBinding(JButton button, String key) {
+        KeyStroke keyStroke = KeyStroke.getKeyStroke(key);
+        button.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(keyStroke, key);
+        button.getActionMap().put(key, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                button.doClick();
+            }
+        });
+    }
+
+    //Main method for the form
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CalculatorFace");
+        frame.setContentPane(new CalculatorFace().Calculator);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
